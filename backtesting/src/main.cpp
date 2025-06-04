76102502bf195ffd33641126c93fce2c3047a147
Kyan Van den Eynde <kyanvde@gmail.com>
2025-06-04 23:46:07 +0200

    add cpr dependency and update example .env

--- backtesting/src/main.cpp
+++ backtesting/src/main.cpp
@@ -7,6 +7,7 @@ int main() {
     const core::OHLCBar bar(std::chrono::system_clock::now(), 100.5, 105.0, 99.9, 104.2, 1500);
 
     std::cout << bar << std::endl;
+    
 
     return EXIT_SUCCESS;
 }
